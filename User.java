347cc5f965aa6c83561f34b0a370f60db862ed40
hanifekurnaz <hanife.kurnaz@bil.omu.edu.tr>
2016-12-28 15:26:19 +0200

    Create User.java

---
+++ User.java
@@ -0,0 +1,46 @@
+package com.model;
+
+public class User {
+	private int userID;
+	private String username; 
+	private String password;
+	private int yetki;
+	private String ad;
+	private String soyad;
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getYetki() {
+		return yetki;
+	}
+	public void setYetki(int yetki) {
+		this.yetki = yetki;
+	}
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	public String getSoyad() {
+		return soyad;
+	}
+	public void setSoyad(String soyad) {
+		this.soyad = soyad;
+	}
+}
